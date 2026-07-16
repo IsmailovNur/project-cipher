230b394b226e21d0beaf11aec29b3ea056c8e290
IsmailovNurmuhammed <ismailovnurmuhammed@gmail.com>
2026-07-16 12:33:18 +0600

    install mui dep-s, add base layout for app with MUI, add dark them provider, create AxiosApi.ts

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,72 @@
 import './App.css';
+import { useState } from "react";
+import {
+  Card,
+  Container,
+  IconButton,
+  Stack,
+  TextField,
+  Typography
+} from '@mui/material';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 const App = () => {
+
+  const [decoded, setDecoded] = useState('');
+  const [encoded, setEncoded] = useState('');
+  const [password, setPassword] = useState('');
+
+
   return (
     <div className="App">
+      <Typography
+        sx={{textAlign: 'center', color: 'text.secondary', mt: 4}}
+        variant="h2"
+      >Message Viginer Chiper
+      </Typography>
 
+      <Container maxWidth="sm" style={{marginTop: '50px'}}>
+        <Stack spacing={3}>
+
+          <TextField
+            label="Decoded message"
+            multiline
+            rows={4}
+            type="text"
+            fullWidth
+            value={decoded}
+            onChange={(e) => setDecoded(e.target.value)}
+          />
+
+          <Card sx={{display: 'flex', alignItems: 'center', gap: 2}}>
+            <TextField
+              placeholder="Password"
+              type="text"
+              fullWidth
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <IconButton color="primary" size="large">
+              <ArrowDownwardIcon />
+            </IconButton>
+
+            <IconButton color="primary" size="large">
+              <ArrowUpwardIcon />
+            </IconButton>
+          </Card>
+
+          <TextField
+            label="Encoded message"
+            multiline
+            rows={4}
+            fullWidth
+            value={encoded}
+            onChange={(e) => setEncoded(e.target.value)}
+          />
+
+        </Stack>
+      </Container>
     </div>
   )
 }
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,9 +4,19 @@ import { store } from "./app/store.ts";
 import App from "./App.tsx";
 
 import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
+    <ThemeProvider theme={darkTheme}>
     <App />
+    </ThemeProvider>
   </Provider>,
 );
